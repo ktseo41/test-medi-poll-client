bd118a7a958b4110d22f5344935a361f513d3d5c
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-05-31 02:10:54 +0900

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
-  <div id="app">
-    <open-survey-creator />
-    <open-survey />
-  </div>
+  <v-app>
+    <div id="app">
+      <open-survey-creator />
+      <open-survey />
+    </div>
+  </v-app>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import MediPoll from 'medi-poll'
-import 'medi-poll/src/client/dist/medi-poll.css'
-// import MediPoll from '../../../medistream/medi-poll/dist/medi-poll.umd.min.js'
-// import '../../../medistream/medi-poll/dist/medi-poll.css'
+import 'medi-poll/dist/medi-poll.css'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(MediPoll)
 
 new Vue({
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
