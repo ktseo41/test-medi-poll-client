2774c61046e75c7bf9fff8c0a70f0af4396d2dac
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2021-05-31 01:59:13 +0900

    apply medi-poll

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <open-survey-creator />
+    <open-survey />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import MediPoll from 'medi-poll'
+import 'medi-poll/src/client/dist/medi-poll.css'
+// import MediPoll from '../../../medistream/medi-poll/dist/medi-poll.umd.min.js'
+// import '../../../medistream/medi-poll/dist/medi-poll.css'
 
 Vue.config.productionTip = false
+Vue.use(MediPoll)
 
 new Vue({
   render: h => h(App),
